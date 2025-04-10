e06d3c94ae226d9956215a57e8e87629d4f6855d
dam <dam@203P29>
2025-04-10 11:28:32 +0200

    Rama PRODUCCION

---
+++ src/abrilrodriguez/Clase2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abrilrodriguez;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public void visualizarRama(){
+        System.out.println("Estamos en la rama PRODUCCION");
+    }
+}
