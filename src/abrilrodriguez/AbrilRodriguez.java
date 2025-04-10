292dec047eedda7c7a6ad3d7b09a739aabde8c00
dam <dam@203P29>
2025-04-10 11:20:32 +0200

    Primer commit

---
+++ src/abrilrodriguez/AbrilRodriguez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package abrilrodriguez;
+
+/**
+ *
+ * @author dam
+ */
+public class AbrilRodriguez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/abrilrodriguez/Clase1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abrilrodriguez;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+    public void visualizarNombre(){
+        System.out.println("Gonzalo Rodriguez Fernandez");
+    }
+}
